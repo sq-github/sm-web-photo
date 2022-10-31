afb790521f4485046683a5373885bc48dbebfb7d
suquan <suquan@shangminet.net>
2022-10-31 09:30:25 +0800

    feat；新加李伟锋、蒙天爱、曹泽照片

--- pubnish/photoData.js
+++ pubnish/photoData.js
@@ -1,4 +1,7 @@
 var photoData = [
-  { src: "./imgs/zyh/20221021.jpg", time: "2022.10.21" },
-  { src: "./imgs/yyy/20221021.jpg", time: "2022.10.21" },
+  { src: "./imgs/zyh/zyh20221021.jpg", time: "2022.10.21" },
+  { src: "./imgs/yyy/yyy20221021.jpg", time: "2022.10.21" },
+  { src: "./imgs/lwf/lwf20221028.jpg", time: "2022.10.28" },
+  { src: "./imgs/mta/mta20221028.jpg", time: "2022.10.28" },
+  { src: "./imgs/czk/czk20221028.jpg", time: "2022.10.28" },
 ];

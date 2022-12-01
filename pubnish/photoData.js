85db8e96156ffcc048488c18521966e82042b0f6
suquan <suquan@shangminet.net>
2022-12-01 16:35:15 +0800

    fix；增加照片

--- pubnish/photoData.js
+++ pubnish/photoData.js
@@ -6,4 +6,5 @@ var photoData = [
   { src: "./imgs/czk/czk20221028.jpg", time: "2022.10.28" },
   { src: "./imgs/lwf/lwf20221104.jpg", time: "2022.11.04" },
   { src: "./imgs/wxy/wxy20221104.jpg", time: "2022.11.04" },
+  { src: "./imgs/sq/sq20221118.jpg", time: "2022.11.18" },
 ];
